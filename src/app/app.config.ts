203470a66829ba7902071b4e21740c6c0b9ac746
Mauricio Delgado <mauricio.a.delgado@accenture.com>
2026-03-16 15:31:26 -0600

    firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { firebaseConfig } from '../environments/firebase.config';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideDatabase(() => getDatabase())
+
   ]
 };
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcRjvBYlj5DSfgfDOSJvzs2QT1x60H7Ho",
+  authDomain: "gp-app-29916.firebaseapp.com",
+  databaseURL: "https://gp-app-29916-default-rtdb.firebaseio.com",
+  projectId: "gp-app-29916",
+  storageBucket: "gp-app-29916.firebasestorage.app",
+  messagingSenderId: "689720837962",
+  appId: "1:689720837962:web:c8fbb3f27321869501a3b3"
+};
