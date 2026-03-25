c706890115616436e8f368db1e49fce5467af1c6
Mauricio Delgado <mauricio.a.delgado@accenture.com>
2026-03-25 00:27:30 -0600

    http url dynamic update && new endpoint communication

--- src/app/services/http.ts
+++ src/app/services/http.ts
@@ -8,15 +8,20 @@ import { Observable } from 'rxjs';
 export class Http {
 
   constructor(private http: HttpClient) {}
-private apiUrl = 'https://gp-app-29916-default-rtdb.firebaseio.com/empleados.json';
+private apiUrl = 'https://gp-app-29916-default-rtdb.firebaseio.com';
 
 crearPost(data: any): Observable<any> {
   console.log('crear empleado activado');
-  return this.http.post(this.apiUrl, data); // ahora sí va al endpoint correcto
+  return this.http.post(this.apiUrl+'/empleados.json', data);
 }
 
 getData(): Observable<any> {
   return this.http.get(this.apiUrl); // devuelve todos los empleados
+}
+
+createAdviser(data: any): Observable<any> {
+  console.log('crear asesor');
+  return this.http.post(this.apiUrl+'/asesores.json', data); 
 }
   
 }
