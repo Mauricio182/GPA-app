f5b8b1300498b3cf2b336ace26e818960f705d1b
Mauricio Delgado <mauricio.a.delgado@accenture.com>
2026-03-25 02:42:24 -0600

    add current user to navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { CommonModule, UpperCasePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  currentUser: any
+  constructor(private _auth: AuthService, private router: Router){
+this.currentUser= this._auth.currentUser
+
+console.warn('valor del current user',this._auth.currentUser.nombre)
+  }
+
+singOut(){
+  this._auth.logout()
+  this.router.navigate(['/home-login']);
+}
 
 }
