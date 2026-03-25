e7ff1813fafd8dc915511c63ae1f2f2c1e1ffcf9
Mauricio Delgado <mauricio.a.delgado@accenture.com>
2026-03-25 02:45:36 -0600

    add current user to auth service

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -12,6 +12,7 @@ export class AuthService {
   isAuthenticated = this._isAuthenticated.asReadonly();
     userList: any[] = [];
 
+    currentUser: any
 
  constructor(private http: HttpClient) {
     this.http.get<any[]>('https://gp-app-29916-default-rtdb.firebaseio.com/asesores/.json').subscribe(
@@ -25,17 +26,44 @@ export class AuthService {
     );
   }
 
-  login(email: any, password: any): boolean {
+  // login(email: any, password: any): boolean {
+    
 
-    if (Object.values(this.userList).some(user => user.email === email && user.password === password)) {
-      this._isAuthenticated.set(true);
-     console.warn('llaves correctas')
-      return true;
-    }
-    return false;
+  //   if (Object.values(this.userList).some(user => user.email === email && user.password === password)) {
+  //     this._isAuthenticated.set(true);
+  //    console.warn('llaves correctas')
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  login(email: any, password: any) {
+  // Buscamos el usuario con las credenciales correctas
+  const usuarioEncontrado = Object.values(this.userList).find(
+    user => user.email === email && user.password === password
+  );
+
+  if (usuarioEncontrado) {
+    // Guardamos solo las propiedades que nos interesan
+    this.currentUser = {
+      nombre: usuarioEncontrado.name,
+      apellido: usuarioEncontrado.lastName,
+      puesto: usuarioEncontrado.rol
+    };
+    console.warn('======================',this.currentUser)
+
+    this._isAuthenticated.set(true);
+    console.warn('Llaves correctas');
+    return true;
   }
+ console.warn('Llaves incorrectas');
+  return false; // credenciales incorrectas
+  
+}
+  
 
   logout() {
     this._isAuthenticated.set(false);
+    console.log('close sesion')
   }
 }
